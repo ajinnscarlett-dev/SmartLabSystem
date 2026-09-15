219f46ac3c48248a54123efa714fc05be7c99bfa
ajinnscarlett-dev <ajinnscarlett@gmail.com>
2026-09-16 00:15:25 +0800

    feat(network): start machine presence with client lifecycle

--- SmartLab.Client/App.xaml.cs
+++ SmartLab.Client/App.xaml.cs
@@ -1,14 +1,23 @@
-﻿using System.Configuration;
-using System.Data;
+using System;
 using System.Windows;
 
 namespace SmartLab.Client
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        private readonly MachinePresenceService _machinePresenceService =
+            new MachinePresenceService();
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _machinePresenceService.Start();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _machinePresenceService.Dispose();
+            base.OnExit(e);
+        }
+    }
 }
