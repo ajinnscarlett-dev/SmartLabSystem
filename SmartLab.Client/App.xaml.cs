f3fc1c561ff59c006c80f2c3baef10cf27d220ad
ajinnscarlett-dev <ajinnscarlett@gmail.com>
2026-09-16 02:09:58 +0800

    fix: use explicit WPF application startup

--- SmartLab.Client/App.xaml.cs
+++ SmartLab.Client/App.xaml.cs
@@ -1,60 +1,30 @@
 using System;
 using System.Windows;
-using System.Windows.Threading;
 
 namespace SmartLab.Client
 {
     public partial class App : Application
     {
         private MachinePresenceService? _machinePresenceService;
-        private DispatcherTimer? _windowLifetimeTimer;
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            ShutdownMode = ShutdownMode.OnLastWindowClose;
+
             base.OnStartup(e);
 
             _machinePresenceService = new MachinePresenceService();
             _machinePresenceService.Start();
 
-            _windowLifetimeTimer = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromMilliseconds(250)
-            };
-
-            _windowLifetimeTimer.Tick += WindowLifetimeTimer_Tick;
-            _windowLifetimeTimer.Start();
-        }
-
-        private void WindowLifetimeTimer_Tick(
-            object? sender,
-            EventArgs e)
-        {
-            bool hasVisibleWindow = false;
-
-            foreach (Window window in Windows)
-            {
-                if (window.IsVisible)
-                {
-                    hasVisibleWindow = true;
-                    break;
-                }
-            }
-
-            if (!hasVisibleWindow)
-            {
-                Shutdown();
-            }
+            MainWindow loginWindow = new MainWindow();
+            MainWindow = loginWindow;
+            loginWindow.Show();
         }
 
         protected override void OnExit(ExitEventArgs e)
         {
-            _windowLifetimeTimer?.Stop();
-            _windowLifetimeTimer = null;
-
             _machinePresenceService?.Dispose();
             _machinePresenceService = null;
-
             base.OnExit(e);
         }
     }
