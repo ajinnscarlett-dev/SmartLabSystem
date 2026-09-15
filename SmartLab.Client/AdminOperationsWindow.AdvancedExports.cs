0868b8cd583587e1b51bdd902fe8e7c6ceb26d51
ajinnscarlett-dev <ajinnscarlett@gmail.com>
2026-09-16 00:54:30 +0800

    fix: restore advanced export client extensions

--- SmartLab.Client/AdminOperationsWindow.AdvancedExports.cs
+++ SmartLab.Client/AdminOperationsWindow.AdvancedExports.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
