cad5ce02454b3ec39ea89a26a089b89d94777dbf
ajinnscarlett-dev <ajinnscarlett@gmail.com>
2026-09-16 02:15:58 +0800

    fix: surface WPF client runtime exceptions instead of silent exit

--- SmartLab.Client/App.xaml.cs
+++ SmartLab.Client/App.xaml.cs
@@ -1,5 +1,8 @@
 using System;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace SmartLab.Client
 {
@@ -11,6 +14,10 @@ namespace SmartLab.Client
         {
             ShutdownMode = ShutdownMode.OnLastWindowClose;
 
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+            TaskScheduler.UnobservedTaskException += App_UnobservedTaskException;
+            AppDomain.CurrentDomain.UnhandledException += App_UnhandledException;
+
             base.OnStartup(e);
 
             _machinePresenceService = new MachinePresenceService();
@@ -19,6 +26,69 @@ namespace SmartLab.Client
             MainWindow loginWindow = new MainWindow();
             MainWindow = loginWindow;
             loginWindow.Show();
+        }
+
+        private void App_DispatcherUnhandledException(
+            object sender,
+            DispatcherUnhandledExceptionEventArgs e)
+        {
+            LogClientError("DispatcherUnhandledException", e.Exception);
+
+            MessageBox.Show(
+                "SmartLab encountered an unexpected error and remained open.\n\n" +
+                e.Exception.Message +
+                "\n\nSee the client error log for details.",
+                "SmartLab Client Error",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+
+            e.Handled = true;
+        }
+
+        private static void App_UnobservedTaskException(
+            object? sender,
+            UnobservedTaskExceptionEventArgs e)
+        {
+            LogClientError("UnobservedTaskException", e.Exception);
+            e.SetObserved();
+        }
+
+        private static void App_UnhandledException(
+            object sender,
+            UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception exception)
+            {
+                LogClientError("AppDomainUnhandledException", exception);
+            }
+        }
+
+        private static void LogClientError(
+            string source,
+            Exception exception)
+        {
+            try
+            {
+                string folder = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    "SmartLab");
+
+                Directory.CreateDirectory(folder);
+
+                string path = Path.Combine(
+                    folder,
+                    "ClientError.log");
+
+                File.AppendAllText(
+                    path,
+                    $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {source}\n" +
+                    exception +
+                    "\n\n");
+            }
+            catch
+            {
+                // Never let error logging terminate the client.
+            }
         }
 
         protected override void OnExit(ExitEventArgs e)
