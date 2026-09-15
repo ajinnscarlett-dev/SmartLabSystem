bcaabfe8f4013e58816be385e6fd166a254b4dbf
ajinnscarlett-dev <ajinnscarlett@gmail.com>
2026-09-16 02:06:35 +0800

    fix: keep WPF client alive during window transitions

--- SmartLab.Client/App.xaml.cs
+++ SmartLab.Client/App.xaml.cs
@@ -1,24 +1,60 @@
 using System;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace SmartLab.Client
 {
     public partial class App : Application
     {
         private MachinePresenceService? _machinePresenceService;
+        private DispatcherTimer? _windowLifetimeTimer;
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            ShutdownMode = ShutdownMode.OnExplicitShutdown;
             base.OnStartup(e);
 
             _machinePresenceService = new MachinePresenceService();
             _machinePresenceService.Start();
+
+            _windowLifetimeTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(250)
+            };
+
+            _windowLifetimeTimer.Tick += WindowLifetimeTimer_Tick;
+            _windowLifetimeTimer.Start();
+        }
+
+        private void WindowLifetimeTimer_Tick(
+            object? sender,
+            EventArgs e)
+        {
+            bool hasVisibleWindow = false;
+
+            foreach (Window window in Windows)
+            {
+                if (window.IsVisible)
+                {
+                    hasVisibleWindow = true;
+                    break;
+                }
+            }
+
+            if (!hasVisibleWindow)
+            {
+                Shutdown();
+            }
         }
 
         protected override void OnExit(ExitEventArgs e)
         {
+            _windowLifetimeTimer?.Stop();
+            _windowLifetimeTimer = null;
+
             _machinePresenceService?.Dispose();
             _machinePresenceService = null;
+
             base.OnExit(e);
         }
     }
